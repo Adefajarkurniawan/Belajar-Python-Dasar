b71d79d2c77970d2bb719624dfa7436be6ebb365
Adefajarkurniawan <adefajar876@gmail.com>
2025-06-03 14:07:18 +0800

    Source Code Pembelajaran

---
+++ Episode 1 - Variabel dan Tipe Data/variabel-tipeData.py
@@ -0,0 +1,12 @@
+# Tipe data dasar di Python 
+nama = "Bunie" # string 
+umur = 20 # integer 
+tinggi = 160.5 # float 
+is_mahasiswa = True #boolean 
+
+
+# Menampilkan semua nilai variabel 
+print("Nama:", nama) 
+print("Umur:", umur) 
+print("Tinggi badan:", tinggi) 
+print("Apakah mahasiswa?", is_mahasiswa)
---
+++ Episode 4 - Operasi Aritmatika/aritmatika.py
@@ -0,0 +1,98 @@
+#Aritmatika 
+a = 10
+b = 3
+
+hasil_tambah = a + b
+print("Penjumlahan (a + b):", hasil_tambah)  # Output: 13
+
+# Pengurangan
+hasil_kurang = a - b
+print("Pengurangan (a - b):", hasil_kurang)  # Output: 7
+
+# Perkalian
+hasil_kali = a * b
+
+
+print("Perkalian (a * b):", hasil_kali)  # Output: 30
+
+# Pembagian
+hasil_bagi = a / b
+print("Pembagian (a / b):", hasil_bagi)  # Output: 3.333...
+
+# Pembagian Bulat
+hasil_bagi_bulat = a // b
+print("Pembagian Bulat (a // b):", hasil_bagi_bulat)  # Output: 3
+
+# Sisa Bagi (Modulo)
+hasil_modulo = a % b
+print("Sisa Bagi (a % b):", hasil_modulo)  # Output: 1
+
+# Pangkat
+hasil_pangkat = a ** b
+print("Pangkat (a ** b):", hasil_pangkat)  # Output: 1000
+
+
+
+
+
+#Perbandingan
+x = 7
+y = 5
+
+# Sama dengan
+print("x == y:", x == y)       # False
+
+# Tidak sama dengan
+print("x != y:", x != y)       # True
+
+# Kurang dari
+print("x < y:", x < y)         # False
+
+# Lebih dari
+print("x > y:", x > y)         # True
+
+# Kurang dari atau sama dengan
+print("x <= y:", x <= y)       # False
+
+# Lebih dari atau sama dengan
+print("x >= y:", x >= y)       # True
+
+
+
+
+
+
+#logika
+# Operasi Logika di Python
+
+x = 10
+y = 5
+
+# and: True jika kedua kondisi True
+print("x > 5 and y < 10:", x > 5 and y < 10)  # True and True => True
+
+# or: True jika salah satu kondisi True
+print("x < 5 or y < 10:", x < 5 or y < 10)    # False or True => True
+
+# not: Membalik nilai boolean
+print("not(x == y):", not(x == y))            # not(False) => True
+
+
+
+
+
+#penugasan
+x = 10        # Menyimpan nilai 10 ke variabel x
+print("x =", x)  # Output: 10
+
+x += 5        # x = x + 5 → x sekarang 15
+print("x += 5 ->", x)
+
+x -= 3        # x = x - 3 → x sekarang 12
+print("x -= 3 ->", x)
+
+x *= 2        # x = x * 2 → x sekarang 24
+print("x *= 2 ->", x)
+
+x /= 4        # x = x / 4 → x sekarang 6.0
+print("x /= 4 ->", x)
